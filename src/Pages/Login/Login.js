b468318d9310568af6f89164badf931c7501564b
Pedro <walme@LAPTOP-13PJ4Q0V>
2019-09-10 13:56:35 -0300

    Layout tela de login

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,6 +1,20 @@
 import React from "react";
+import "./Login.css";
 import logo from "../../assets/img/event.svg";
 
 export default function Login() {
-  return <img src={logo} alt="BackOffice" />;
+  return (
+    <div className="login-container">
+      <form>
+        <img src={logo} alt="BackOffice" />
+        <input placeholder="Insira o seu email" />
+        <input type="password" placeholder="Insira sua senha" />
+
+        <button type="submit ">Entrar</button>
+        <div className="span-text">
+          <span>Esqueci Senha</span>
+        </div>
+      </form>
+    </div>
+  );
 }
