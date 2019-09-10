0b206fc05d262814069216647a658afeeca33111
Pedro <walme@LAPTOP-13PJ4Q0V>
2019-09-10 14:28:29 -0300

    Inicio useState

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/img/event.svg";
 
 export default function Login() {
+  const [userEmail, setUserEmail] = useState("");
+  const [userPassword, setPassword] = useState("");
+
+  function logar(e) {
+    e.preventDefault();
+
+    console.log(userEmail, userPassword);
+  }
+
   return (
     <div className="login-container">
-      <form>
+      <form onSubmit={logar}>
         <img src={logo} alt="BackOffice" />
-        <input placeholder="Insira o seu email" />
-        <input type="password" placeholder="Insira sua senha" />
+        <input
+          placeholder="Insira o seu email"
+          value={userEmail}
+          onChange={e => setUserEmail(e.target.value)}
+        />
+        <input
+          type="password"
+          placeholder="Insira sua senha"
+          value={userPassword}
+          onChange={e => setPassword(e.target.value)}
+        />
 
         <button type="submit ">Entrar</button>
         <div className="span-text">
