ceacaa64d1105f654e9d8a5dcf318c3b2bd1c9ef
Pedro <walme@LAPTOP-13PJ4Q0V>
2019-09-10 17:14:44 -0300

    formulario de cadastro

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -2,6 +2,14 @@ import React, { useState } from "react";
 import "./Login.css";
 import logo from "../../assets/img/event.svg";
 
+//MATERIAL-UI
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
 export default function Login({ history }) {
   const [userEmail, setUserEmail] = useState("");
   const [userPassword, setPassword] = useState("");
@@ -16,27 +24,83 @@ export default function Login({ history }) {
     console.log(userEmail, userPassword);
   }
 
-  return (
-    <div className="login-container">
-      <form onSubmit={logar}>
-        <img src={logo} alt="BackOffice" />
-        <input
-          placeholder="Insira o seu email"
-          value={userEmail}
-          onChange={e => setUserEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Insira sua senha"
-          value={userPassword}
-          onChange={e => setPassword(e.target.value)}
-        />
+  function goToregister() {
+    const id = 1;
 
-        <button type="submit ">Entrar</button>
-        <div className="span-text">
-          <span>Esqueci Senha</span>
-        </div>
-      </form>
-    </div>
+    history.push("/registerUser");
+  }
+
+  // return (
+  //   <div className="login-container">
+  //     <form onSubmit={logar}>
+  //       <img src={logo} alt="BackOffice" />
+  //       <input
+  //         placeholder="Insira o seu email"
+  //         value={userEmail}
+  //         onChange={e => setUserEmail(e.target.value)}
+  //       />
+  //       <input
+  //         type="password"
+  //         placeholder="Insira sua senha"
+  //         value={userPassword}
+  //         onChange={e => setPassword(e.target.value)}
+  //       />
+
+  //       <button type="submit " className="button-primary">
+  //         Entrar
+  //       </button>
+  //       <div className="span-text">
+  //         <span onClick={goToregister}>Cadastre-se</span>
+  //       </div>
+  //     </form>
+  //   </div>
+  // );
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className="container-register-user">
+        <Typography component="h1" variant="h5">
+          Login
+        </Typography>
+        <form className="form" onSubmit={logar}>
+          <Grid container spacing={2}>
+            <Grid item xs={12}>
+              <TextField
+                autoFocus
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+                value={userEmail}
+                onChange={e => setUserEmail(e.target.value)}
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={userPassword}
+                onChange={e => setPassword(e.target.value)}
+              />
+            </Grid>
+          </Grid>
+          <Button type="submit" fullWidth className="button-primary">
+            Entrar
+          </Button>
+          <Grid className="span-text">
+            <span onClick={goToregister}>Cadastre-se</span>
+          </Grid>
+        </form>
+      </div>
+    </Container>
   );
 }
---
+++ src/Pages/RegisterUser/RegisterUser.js
@@ -0,0 +1,93 @@
+import React from "react";
+import "./RegisterUser.css";
+
+//MATERIAL-UI
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Grid from "@material-ui/core/Grid";
+import Typography from "@material-ui/core/Typography";
+import Container from "@material-ui/core/Container";
+
+export default function RegisterUser() {
+  return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <div className="container-register-user">
+        <Typography component="h1" variant="h5">
+          Cadastro
+        </Typography>
+        <form className="form" noValidate>
+          <Grid container spacing={2}>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                autoComplete="fname"
+                name="firstName"
+                variant="outlined"
+                required
+                fullWidth
+                id="firstName"
+                label="Nome"
+                autoFocus
+              />
+            </Grid>
+            <Grid item xs={12} sm={6}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="lastName"
+                label="Sobrenome"
+                name="lastName"
+                autoComplete="lname"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="cpf"
+                label="CPF"
+                name="cpf"
+                autoComplete="cpf"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                id="email"
+                label="Email"
+                name="email"
+                autoComplete="email"
+              />
+            </Grid>
+            <Grid item xs={12}>
+              <TextField
+                variant="outlined"
+                required
+                fullWidth
+                name="password"
+                label="Senha"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+              />
+            </Grid>
+          </Grid>
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            color="primary"
+            className="button-primary"
+          >
+            Cadastrar
+          </Button>
+        </form>
+      </div>
+    </Container>
+  );
+}
